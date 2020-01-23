10f8042387904cb3eafe1ad1e6893579f17546b2
Slooowpoke <malii.coxon@gmail.com>
2020-01-23 18:02:45 +0000

    Added basic styles, framer motion, adjusted structure and updated Readme

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export default () => (
+  <div className="app">
+    <header className="header">
+      <h1>{"Smartzer Coding Test"}</h1>
+    </header>
+    <section className={"body"}>
+      <div className={"pretend-video-player"}>
+
+      </div>
+    </section>
+  </div>
+);
