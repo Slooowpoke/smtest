fda1d6bf80bf1b333785d5ef9f3354ca5d6b512a
Slooowpoke <malii.coxon@gmail.com>
2020-01-23 18:47:53 +0000

    Finished product modal, gallery and responsiveness

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,9 +1,34 @@
 import React, { useState } from 'react';
 
 export default ({  }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(1);
+
+  const nextImage = () => {
+    if(currentImageIndex < 4){
+      setCurrentImageIndex(currentImageIndex+1);
+    }
+  };
+
+  const previousImage = () => {
+    if(currentImageIndex > 1){
+      setCurrentImageIndex(currentImageIndex-1);
+    }
+  }
+
   return (
     <div className={"gallery"}>
-
+      <div className={"images-wrapper"}>
+        <div className={"image-wrapper"} style={{backgroundImage: `url(/product-images/img${currentImageIndex}.png)`}}>
+        </div>
+        <div className={"desktop-side-images"}>
+          <img src={"/product-images/img1.png"} onClick={() => setCurrentImageIndex(1)} />
+          <img src={"/product-images/img2.png"} onClick={() => setCurrentImageIndex(2)} />
+          <img src={"/product-images/img3.png"} onClick={() => setCurrentImageIndex(3)} />
+          <img src={"/product-images/img4.png"} onClick={() => setCurrentImageIndex(4)} />
+        </div>
+      </div>
+      <button onClick={previousImage}>PreviousImage</button>
+      <button onClick={nextImage}>Next</button>
     </div>
   )
 };
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,18 +4,21 @@ import Gallery from "./Gallery";
 export default ({ data, closeModal }) => {
   const { productName, productPrice, productDescription } = data;
   return (
-    <div className={"product"}>
+    <>
       <div className={"back"} onClick={closeModal}>
         <span>{"BACK TO VIDEO"}</span>
       </div>
-      <Gallery />
-      <div className={"product-description"}>
-        <h4>{productName}</h4>
-        <h4>{productPrice}</h4>
-        <a href={"buy-it"} className={"btn"}>
-          {"BUY NOW"}
-        </a>
+      <div className={"product"}>
+        <Gallery />
+        <div className={"product-description"}>
+          <h4>{productName}</h4>
+          <h4>{productPrice}</h4>
+          <h4 className={"product-details"}>{productDescription}</h4>
+          <a href={"buy-it"} className={"btn"}>
+            {"BUY NOW"}
+          </a>
+        </div>
       </div>
-    </div>
+    </>
   )
 };
